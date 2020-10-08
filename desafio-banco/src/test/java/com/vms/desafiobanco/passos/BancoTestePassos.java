cd68e1120ce85947239da9779224b67e43933aab
victtormotta <victor.mota892@gmail.com>
2020-10-08 14:08:54 -0300

    Remoção de métodos comentados;

--- desafio-banco/src/test/java/com/vms/desafiobanco/passos/BancoTestePassos.java
+++ desafio-banco/src/test/java/com/vms/desafiobanco/passos/BancoTestePassos.java
@@ -19,34 +19,16 @@ public class BancoTestePassos {
     private int totalContas;
     private Double totalDinheiro;
 
-//    @Dado("^que as contas sao do \"(.*?)\"$")
-//    public void que_as_contas_sao_do(String nome, List<Conta> listaDeContas) throws Throwable {
-//        // Definição do banco e associando as contas
-//        banco = new Banco(nome, listaDeContas);
-//
-//    }
-
     @Dado("^que as contas sao do \"([^\"]*)\"$")
     public void queAsContasSaoDo(String nome, List<Conta> listaDeContas) throws Throwable {
         // Definição do banco e associando as contas
         banco = new Banco(nome, listaDeContas);
     }
 
-//    @Dado("^o calculo do total de contas criadas$")
-//    public void o_calculo_do_total_de_contas_criadas() throws Throwable {
-//        totalContas = banco.getListaDeContas().size();
-//    }
-
     @Dado("^o calculo do total de contas criadas$")
     public void oCalculoDoTotalDeContasCriadas() throws Throwable {
         totalContas = banco.getListaDeContas().size();
     }
-
-//    @Entao("^o total de contas e (\\d+)$")
-//    public void o_total_de_contas_e(int totalContasEsperado) throws Throwable {
-//        Assertions.assertEquals(totalContasEsperado, totalContas,
-//                "O cálculo do total de contas está incorreto");
-//    }
 
     @Entao("^o total de contas e (\\d+)$")
     public void oTotalDeContasE(int totalContasEsperado) throws Throwable{
@@ -54,26 +36,11 @@ public class BancoTestePassos {
                 "O cálculo do total de contas está incorreto");
     }
 
-//    @Dado("^o calculo do total de dinheiro$")
-//    public void o_calculo_do_total_de_dinheiro() throws Throwable {
-//        totalDinheiro = banco.getListaDeContas().stream().mapToDouble
-//                //(c -> c.getSaldo()).sum();
-//                (Conta::getSaldo).sum();
-//    }
-
     @Dado("^o calculo do total de dinheiro$")
     public void oCalculoDoTotalDeDinheiro() throws Throwable  {
         totalDinheiro = banco.getListaDeContas().stream().mapToDouble
                 (Conta::getSaldo).sum();
     }
-
-//    @Entao("^o total de dinheiro no banco e (\\d+)$")
-//    public void o_total_de_dinheiro_no_banco_e(Double totalDinheiroEsperado)
-//            throws Throwable {
-//
-//        Assertions.assertEquals(totalDinheiroEsperado, totalDinheiro,
-//                "O cálculo do total de dinheiro no banco " + banco.getNome() + " está incorreto");
-//    }
 
     @Entao("^o total de dinheiro no banco e (\\d+)$")
     public void oTotalDeDinheiroNoBancoE(Double totalDinheiroEsperado) throws Throwable {
