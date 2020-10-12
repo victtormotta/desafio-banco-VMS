0673acc5ab2df2b672a4730dbc4b3ba7a0414d44
victtormotta <victor.mota892@gmail.com>
2020-10-12 11:01:18 -0300

    Definição de rotas;

--- desafio-banco-angular/src/app/app-routing.module.ts
+++ desafio-banco-angular/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {CriarComponent} from './criar/criar.component';
+import {DepositarComponent} from './depositar/depositar.component';
+import {SacarComponent} from './sacar/sacar.component';
+import {TransferirComponent} from './transferir/transferir.component';
 
 const routes: Routes = [
   { path: 'criar', component: CriarComponent },
+  { path: 'depositar', component: DepositarComponent },
+  { path: 'sacar', component: SacarComponent },
+  { path: 'transferir', component: TransferirComponent }
 ];
 
 @NgModule({
